e21c51f605f707b2bb39c80584fba32f84a5af7b
talhatxd <talha.roshaun@txend.com>
2025-07-03 17:25:47 +0500

    Initial setup

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ app.get("/home", (req, res) => {
   res.send("Hello from home route");
 });
 
-app.get("/about", (req, res) => {
-  res.send("Hello from about");
-});
+// app.get("/about", (req, res) => {
+//   res.send("Hello from about");
+// });
 app.listen(3000, () => {
   console.log("Sever is successfully listening at port 3000");
 });
