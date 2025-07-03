8a90465b03d796f729b9cd997a1a0e52ddb5a969
talhatxd <talha.roshaun@txend.com>
2025-07-03 17:16:41 +0500

    Initial commit with just app.js and setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from / app");
+});
+app.get("/home", (req, res) => {
+  res.send("Hello from home route");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello from about");
+});
+app.listen(3000, () => {
+  console.log("Sever is successfully listening at port 3000");
+});
