772fa6113cf73ea2da23afd150fa8c41238a689a
roshaunjunior <talharoshaun@gmail.com>
2025-07-08 20:58:02 +0500

    Query Params and Dynamic Params difference

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from / app");
-});
-app.get("/home", (req, res) => {
-  res.send("Hello from home route");
+// Query Params
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Talha", lastName: "Roshaun" });
 });
 
-app.get("/about", (req, res) => {
-  res.send("Hello from about");
+// Params
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Talha", lastName: "Tariq" });
 });
+// app.get("/", (req, res) => {
+//   res.send("Hello from / app");
+// });
+// app.get("/home", (req, res) => {
+//   res.send("Hello from home route");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("Hello from about");
+// });
 app.listen(3000, () => {
   console.log("Sever is successfully listening at port 3000");
 });
