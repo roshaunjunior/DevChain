ef47b48bbff32570faa93ce3d7f060b4ca9c0a92
roshaunjunior <talharoshaun@gmail.com>
2025-08-04 18:38:18 +0500

    Middleware practice

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const app = express();
 
 // Query Params
@@ -24,6 +26,25 @@ app.get("/user/:userId", (req, res) => {
 // app.get("/about", (req, res) => {
 //   res.send("Hello from about");
 // });
+
+// We can add multiple router handlers function.
+// Use next() to jump to the next function.
+// once res.send() "Response sent to client" than on second time it will give an error that  Cannot set headers after they are sent to the client
+// So only once res.send should be used but we can use multiple handlers as middleware to use for different functionality.
+
+app.use(
+  "/home",
+  (req, res, next) => {
+    console.log("home 1");
+    // res.send("Resposne from Home 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Home 2");
+    res.send("Resposne from Home 2");
+  }
+);
+
 app.listen(3000, () => {
   console.log("Sever is successfully listening at port 3000");
 });
